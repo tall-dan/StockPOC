91eb29a362dd6aafd2f221b3c1145894d3f5f95c
schepedw <schepedw@rose-hulman.edu>
2013-07-31 09:10:31 -0500

    added statements to create the DB

---
+++ CreateDB.sql
@@ -0,0 +1,58 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS `Transaction_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Transaction_history` (
+  `Ticker_name` varchar(5) DEFAULT NULL,
+  `Price` decimal(10,0) DEFAULT NULL,
+  `Shares` int(11) DEFAULT NULL,
+  `BUY_SELL` varchar(10) DEFAULT NULL,
+  `Profit` decimal(10,0) DEFAULT NULL,
+  `Time` datetime DEFAULT NULL,
+  `Entry_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`Entry_id`),
+  KEY `fk_Transaction_history_1_idx` (`Ticker_name`),
+  CONSTRAINT `fk_Transaction_history_1` FOREIGN KEY (`Ticker_name`) REFERENCES `stocks` (`Ticker_name`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `stocks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stocks` (
+  `Ticker_name` varchar(5) NOT NULL,
+  `BuyPrice` decimal(10,0) NOT NULL,
+  `Shares` decimal(10,0) NOT NULL,
+  `Max_loss` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`Ticker_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `ticker_prices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ticker_prices` (
+  `Ticker_name` varchar(5) NOT NULL,
+  `Price` decimal(10,0) NOT NULL,
+  `Time` datetime NOT NULL,
+  `EntryID` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`EntryID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
